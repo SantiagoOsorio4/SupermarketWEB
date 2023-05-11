6f6ccb28ff9126fd4353f7e55acceac0affb78db
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-05-11 15:50:38 -0500

    Delete  Categories

---
+++ Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using System.Security.Cryptography;
+
+namespace SupermarketWEB.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+        public DeleteModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Category Category { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetasync(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+        var category = await _context.Categories.FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Category = category;
+            }
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category != null)
+            {
+                Category = category;
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage("./Index");
+        }
+    } 
+}
