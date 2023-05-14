b8ef614a3f7d31c7a38c5e7c7708f998f2602d06
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-05-14 02:11:51 -0500

    Create Products

--- Models/Provider.cs
+++ Models/Provider.cs
@@ -7,12 +7,12 @@ namespace SupermarketWEB.Models
         public int Id { get; set; }
         [Required]
         [MinLength(2)]
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
         [Required]
         [EmailAddress]
-        public string Email { get; set; }
+        public string? Email { get; set; }
         [Required]
-        public string PhoneNumber { get; set; }
-        public string Company { get; set; } 
+        public string? PhoneNumber { get; set; }
+        public string? Company { get; set; } 
     }
 }
