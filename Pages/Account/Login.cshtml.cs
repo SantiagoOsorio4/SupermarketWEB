c3b85c63dc895b2d5f6b0e1d9a4bb857ae769131
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-05-19 15:32:19 -0500

    Creando login

---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SupermarketWEB.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public User User { get; set; }
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPostAsync() 
+        {
+            if (!ModelState.IsValid) return Page();
+
+            if (User.Email == "correo@gmail.com" && User.Password == "12345") 
+            {
+                return RedirectToPage("/index");
+            }
+            return Page();
+        }    
+    }
+}
