69ec31e4e41791ad493fc586db5df6cd6619c4a5
SantiagoOsorio4 <santiagoosoriocordoba@gmail.com>
2023-05-19 16:14:16 -0500

    Autenticación 2

--- Data/SupermarketContext.cs
+++ Data/SupermarketContext.cs
@@ -14,6 +14,5 @@ namespace SupermarketWEB.Data
         public DbSet<Provider> Providers { get; set; }
         public DbSet<PayMode> PayModes { get; set; }
         public DbSet<Customer> Customers { get; set; }
-        public DbSet<User> Users { get; set; }
     }
 }
